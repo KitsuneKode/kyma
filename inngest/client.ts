7666d45b0f77d2207144370ff0e6c1ab36260f41
Manash Pratim Bhuyan <bhuyanmanash2002@gmail.com>
2026-04-22 04:18:36 +0530

    Add interview processing pipeline and AI review UI primitives

--- inngest/client.ts
+++
@@ -1,9 +0,0 @@
-import { Inngest } from 'inngest'
-import { serverEnv } from '@/lib/env/server'
-
-export const inngest = new Inngest({
-  id: serverEnv.INNGEST_APP_ID ?? 'kyma',
-  eventKey: serverEnv.INNGEST_EVENT_KEY,
-  signingKey: serverEnv.INNGEST_SIGNING_KEY,
-  isDev: serverEnv.NODE_ENV !== 'production',
-})
